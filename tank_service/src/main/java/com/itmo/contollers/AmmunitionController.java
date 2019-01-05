352aeb9a4d6fe5ca709129f6b7d05b472c15863b
Igor Anokhin <igor.anohin@emlid.com>
2019-01-06 01:55:41 +0300

    AmmunitionController: add ammunition object inside controller to use it in JSF

--- tank_service/src/main/java/com/itmo/contollers/AmmunitionController.java
+++ tank_service/src/main/java/com/itmo/contollers/AmmunitionController.java
@@ -3,7 +3,10 @@ package com.itmo.contollers;
 import com.itmo.services.AmmunitionService;
 import com.pip.entities.Ammunition;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,12 +14,17 @@ import java.util.List;
 import static lombok.AccessLevel.PRIVATE;
 
 @RestController
+@Scope("session")
 @AllArgsConstructor
 @RequestMapping("/ammunitions")
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 public class AmmunitionController {
 
     AmmunitionService ammunitionService;
+
+    @Setter
+    @Getter
+    private Ammunition ammunition = new Ammunition();
 
     @GetMapping("/getAll")
     public List<Ammunition> findAllAmmunitions() {
