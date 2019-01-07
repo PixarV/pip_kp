2f6570d71f1739f098305a1067ad5148e3141c26
ritt <margarita_fedotova@epam.com>
2019-01-07 19:42:26 +0000

    Ammunition: add custom update

--- tank_service/src/main/java/com/itmo/dao/AmmunitionDao.java
+++ tank_service/src/main/java/com/itmo/dao/AmmunitionDao.java
@@ -2,13 +2,25 @@ package com.itmo.dao;
 
 import com.pip.entities.Ammunition;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Query;
 
 @Repository
+@Transactional
 public class AmmunitionDao extends CommonDao<Ammunition> {
     public AmmunitionDao() {
         super(Ammunition.class);
+    }
+
+    public int custom_update(Ammunition ammunition) {
+        Query query = entityManager.createQuery("update Ammunition a set a.callibr = :callibr, " +
+                "a.type = :amm_type, " +
+                "a.breakage = :breakage WHERE a.id = :id");
+        return query.setParameter("callibr", ammunition.getCallibr())
+                .setParameter("amm_type", ammunition.getType())
+                .setParameter("breakage", ammunition.getBreakage())
+                .setParameter("id", ammunition.getId()).executeUpdate();
     }
 
     public int removeAmmunitionById(int id) {
