20f5a580e960419805b5e3ac7cb046366a8dd6cd
ritt <margarita_fedotova@epam.com>
2019-01-08 10:37:22 +0000

    Engine: add custom update

--- firm_service/src/main/java/com/itmo/dao/EngineDao.java
+++ firm_service/src/main/java/com/itmo/dao/EngineDao.java
@@ -1,6 +1,7 @@
 package com.itmo.dao;
 
 import com.pip.entities.Engine;
+import com.pip.entities.Engine;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Query;
@@ -9,6 +10,19 @@ import javax.persistence.Query;
 public class EngineDao extends CommonDao<Engine> {
     public EngineDao() {
         super(Engine.class);
+    }
+
+    public int custom_update(Engine engine) {
+        Query query = entityManager.createQuery("update Engine c set c.fuel = :fuel, " +
+                "c.title = :title, " +
+                "c.power = :power, " +
+                "c.weight = :weight " +
+                "WHERE c.id = :id");
+        return query.setParameter("fuel", engine.getFuel())
+                .setParameter("title", engine.getTitle())
+                .setParameter("power", engine.getPower())
+                .setParameter("weight", engine.getWeight())
+                .setParameter("id", engine.getId()).executeUpdate();
     }
 
     public int removeEngineById(int id) {
