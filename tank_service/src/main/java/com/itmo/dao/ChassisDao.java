9767c1f4130459684d6317607cb999350558c5c6
ritt <margarita_fedotova@epam.com>
2019-01-08 10:15:16 +0000

    Chassis: add custom update

--- tank_service/src/main/java/com/itmo/dao/ChassisDao.java
+++ tank_service/src/main/java/com/itmo/dao/ChassisDao.java
@@ -1,6 +1,7 @@
 package com.itmo.dao;
 
 import com.pip.entities.Chassis;
+import com.pip.entities.Chassis;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Query;
@@ -9,6 +10,21 @@ import javax.persistence.Query;
 public class ChassisDao extends CommonDao<Chassis> {
     public ChassisDao() {
         super(Chassis.class);
+    }
+
+    public int custom_update(Chassis chassis) {
+        Query query = entityManager.createQuery("update Chassis c set c.model = :model, " +
+                "c.title = :title, " +
+                "c.carring = :carring, " +
+                "c.turnSpeed = :turnSpeed, " +
+                "c.weight = :weight " +
+                "WHERE c.id = :id");
+        return query.setParameter("model", chassis.getModel())
+                .setParameter("title", chassis.getTitle())
+                .setParameter("carring", chassis.getCarring())
+                .setParameter("turnSpeed", chassis.getTurnSpeed())
+                .setParameter("weight", chassis.getWeight())
+                .setParameter("id", chassis.getId()).executeUpdate();
     }
 
     public int removeChassisById(int id) {
