b5a682effb169dc600050e69e76eceac82e55636
ritt <margarita_fedotova@epam.com>
2019-01-08 10:27:00 +0000

    Model: @Wither + @Component

--- app/src/main/java/com/pip/entities/Model.java
+++ app/src/main/java/com/pip/entities/Model.java
@@ -6,8 +6,10 @@ import com.pip.util.PostgreSQLEnumType;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.Wither;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,8 +19,10 @@ import java.util.Set;
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
+@Wither
 @Entity
 @Builder
+@Component
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = PRIVATE)
