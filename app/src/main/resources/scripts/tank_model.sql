92f3e2443e6a5554b46f853450c6751ba067b0d3
ritt <margarita_fedotova@epam.com>
2019-01-09 01:17:23 +0000

    Tower: add unique constraint to title

--- app/src/main/resources/scripts/tank_model.sql
+++ app/src/main/resources/scripts/tank_model.sql
@@ -49,7 +49,7 @@ CREATE TABLE chassis (
 
 CREATE TABLE tower (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
+  title VARCHAR(255) UNIQUE NOT NULL,
   armor VARCHAR(255) NOT NULL,
   turn_speed DOUBLE PRECISION NOT NULL CHECK (
     turn_speed >= 0
