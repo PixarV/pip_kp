8b9dd1495fc3a920e1c2b3d86742b293e6860914
ritt <margarita_fedotova@epam.com>
2019-01-10 08:39:01 +0000

    FirmTower: crud check

--- firm_service/src/main/java/com/itmo/dao/FirmTowerDao.java
+++ firm_service/src/main/java/com/itmo/dao/FirmTowerDao.java
@@ -2,10 +2,37 @@ package com.itmo.dao;
 
 import com.pip.entities.FirmTower;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Query;
 
 @Repository
+@Transactional
 public class FirmTowerDao extends CommonDao<FirmTower> {
     public FirmTowerDao() {
         super(FirmTower.class);
+    }
+
+    public int removeFirmTower(int firmId, int towerId) {
+        Query query = entityManager.createQuery("DELETE FROM FirmTower m " +
+                "WHERE m.id_firm = :firm AND m.id_tower = :tower");
+        return query.setParameter("firm", firmId)
+                .setParameter("tower", towerId)
+                .executeUpdate();
+    }
+
+    public int removeFirmFromFtoT(int firmId) {
+        Query query = entityManager.createQuery("DELETE FROM FirmTower m " +
+                "WHERE m.id_firm = :firm");
+        return query.setParameter("firm", firmId)
+                .executeUpdate();
+    }
+
+    public String getTowerSn(int firmId, int towerId) {
+        Query query = entityManager.createNativeQuery("SELECT serial_no FROM firm_tower " +
+                "WHERE id_firm = ? AND id_tower = ?");
+        return (String) query.setParameter(1, firmId)
+                .setParameter(2, towerId)
+                .getSingleResult();
     }
 }
--- firm_service/src/main/java/com/itmo/services/TowerService.java
+++ firm_service/src/main/java/com/itmo/services/TowerService.java
@@ -1,7 +1,11 @@
 package com.itmo.services;
 
+import com.itmo.dao.FirmDao;
+import com.itmo.dao.FirmTowerDao;
 import com.itmo.dao.TowerDao;
 import com.pip.entities.Chassis;
+import com.pip.entities.Firm;
+import com.pip.entities.FirmTower;
 import com.pip.entities.Tower;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.pip.enums.Approve.NOT_APPROVED;
+import static com.pip.enums.FirmSpecializtion.ENGINE;
 import static lombok.AccessLevel.PRIVATE;
 
 @Service
@@ -17,13 +23,28 @@ import static lombok.AccessLevel.PRIVATE;
 public class TowerService {
 
     TowerDao towerDao;
+    FirmDao firmDao;
+    FirmTowerDao firmTowerDao;
 
     public List<Tower> findAllTowers() {
         return towerDao.findAllEntities();
     }
 
-    public void addTower(Tower tower) {
+    public void addTower(Tower tower, int firmId) {
+        Firm firm = firmDao.findEntityById(firmId);
+        if (firm.getSpecialization() == ENGINE) {
+            throw new IllegalStateException("Specialization of firm is incorrect");
+        }
+
+        if (firm.getStatus() == NOT_APPROVED) {
+            throw new IllegalStateException("Firm is not approved");
+        }
         towerDao.saveEntity(tower);
+        FirmTower firmTower = FirmTower.builder()
+                .tower(tower)
+                .firm(firm)
+                .build();
+        firmTowerDao.saveEntity(firmTower);
     }
 
     public void deleteTower(Tower tower) {
@@ -49,5 +70,9 @@ public class TowerService {
 
     public List<Chassis> getChassis(int towerId) {
         return towerDao.getChassis(towerId);
+    }
+
+    public String getTowerSn(int firmId, int towerId) {
+        return firmTowerDao.getTowerSn(firmId, towerId);
     }
 }
