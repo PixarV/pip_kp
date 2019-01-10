724739fc770e174304e0abee0e450a739675b18a
ritt <margarita_fedotova@epam.com>
2019-01-10 02:06:03 +0000

    Firm: refactor

--- firm_service/src/main/java/com/itmo/contollers/FirmController.java
+++ firm_service/src/main/java/com/itmo/contollers/FirmController.java
@@ -5,29 +5,25 @@ import com.pip.entities.Firm;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import static lombok.AccessLevel.PRIVATE;
 
-@Controller
+@RestController
 @AllArgsConstructor
+@RequestMapping("/firms")
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 public class FirmController {
 
     FirmService firmService;
 
     @GetMapping("/findAll")
-    @ResponseBody
     List<Firm> findAllFirms() {
         return firmService.findAllFirms();
     }
 
-    @ResponseBody
     @PostMapping("/reg")
     Firm registrateFirm(@RequestBody Firm firm) {
         firmService.addFirm(firm);
