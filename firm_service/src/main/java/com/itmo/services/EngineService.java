f1f93352fb8b1fc457f45e837fe5a50150ca7bbf
ritt <margarita_fedotova@epam.com>
2019-01-10 08:23:25 +0000

    FirmEngine: crud check

--- firm_service/src/main/java/com/itmo/services/EngineService.java
+++ firm_service/src/main/java/com/itmo/services/EngineService.java
@@ -7,12 +7,16 @@ import com.pip.entities.Engine;
 import com.pip.entities.Firm;
 import com.pip.entities.FirmEngine;
 import com.pip.entities.Model;
+import com.pip.enums.Approve;
+import com.pip.enums.FirmSpecializtion;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.pip.enums.Approve.NOT_APPROVED;
+import static com.pip.enums.FirmSpecializtion.TANK;
 import static lombok.AccessLevel.PRIVATE;
 
 @Service
@@ -29,8 +33,16 @@ public class EngineService {
     }
 
     public void addEngine(Engine engine, int firmId) {
-        engineDao.saveEntity(engine);
         Firm firm = firmDao.findEntityById(firmId);
+        if (firm.getSpecialization() == TANK) {
+            throw new IllegalStateException("Specialization of firm is incorrect");
+        }
+
+        if (firm.getStatus() == NOT_APPROVED) {
+            throw new IllegalStateException("Firm is not approved");
+        }
+
+        engineDao.saveEntity(engine);
         FirmEngine firmEngine = FirmEngine.builder()
                 .engine(engine)
                 .firm(firm)
