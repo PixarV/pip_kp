70e2b72911bbfca01cb92a1d2ed095e554cba3c5
ritt <margarita_fedotova@epam.com>
2019-01-24 01:21:52 +0000

    Human: profile without update

--- human_service/src/main/java/com/itmo/configs/SecurityConfig.java
+++ human_service/src/main/java/com/itmo/configs/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .loginPage("/hello_human.xhtml")
                     .usernameParameter("j_idt4:username")
                     .passwordParameter("j_idt4:password")
-                    .defaultSuccessUrl("/human_control.xhtml", true)
+                    .defaultSuccessUrl("/human_profile.xhtml", true)
                     .permitAll()
                 .and()
                 .logout()
