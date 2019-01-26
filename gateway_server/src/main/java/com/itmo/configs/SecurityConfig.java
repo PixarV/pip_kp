61574ff7f643594d3a892aa705072df66d18d33c
ritt <margarita_fedotova@epam.com>
2019-01-26 01:22:58 +0000

    restructure this shit

--- gateway_server/src/main/java/com/itmo/configs/SecurityConfig.java
+++ gateway_server/src/main/java/com/itmo/configs/SecurityConfig.java
@@ -1,44 +1,61 @@
 package com.itmo.configs;
 
-//
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-////
-////    @Autowired
-////    FirmService firmService;
-////
-////    @Bean
-////    PasswordEncoder noEncoder(){
-////        return NoOpPasswordEncoder.getInstance();
-////    }
-////
-////    @Override
-////    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-////        auth
-////                .userDetailsService(firmService)
-////                .passwordEncoder(noEncoder());
-////    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                    .antMatchers( "/*").permitAll()
-////                    .anyRequest().authenticated()
-//                .and()
-////                .formLogin()
-////                    .loginPage("/hello.xhtml")
-////                    .usernameParameter("j_idt4:username")
-////                    .passwordParameter("j_idt4:password")
-////                    .defaultSuccessUrl("/firms/getAll", true)
-////                    .permitAll()
-////                .and()
-////                .logout()
-////                    .invalidateHttpSession(true)
-////                    .deleteCookies("JSESSIONID")
-////                    .permitAll()
-////                .and()
-//                .csrf().disable();
-//    }
-//}
+
+import com.itmo.services.HumanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    HumanService humanService;
+
+    @Bean
+    PasswordEncoder noEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(humanService)
+                .passwordEncoder(noEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/human_service/hello_human.xhtml", "/human_service/human_reg.xhtml").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/human_service/hello_human.xhtml")
+                    .usernameParameter("j_idt5:username")
+                    .passwordParameter("j_idt5:password")
+                    .defaultSuccessUrl("/human_service/human_profile.xhtml", true)
+                    .permitAll()
+                .and()
+                .logout()
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .permitAll()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/images/**", "/css/**", "/js/**");
+    }
+}
--- tank_service/src/main/java/com/itmo/configs/SecurityConfig.java
+++ tank_service/src/main/java/com/itmo/configs/SecurityConfig.java
@@ -13,8 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/*").permitAll()
+                    .antMatchers("/*").permitAll()
                 .and()
                 .csrf().disable();
     }
+
 }
